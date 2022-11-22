37a886d4a1f665652da5bce593878a47b0167e50
ZUITOPIA <korhwngml123@naver.com>
2022-11-22 17:30:23 +0900

    styled-components, babel 등 기본 설정 및 favicon 변경

---
+++ client/styles/styled.d.ts
@@ -0,0 +1,23 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export type DefaultShadowKey = "normal" | "purple" | "blue";
+  export type DefaultThemeColorKey =
+    | "black"
+    | "white"
+    | "purple"
+    | "blue"
+    | "gray"
+    | "green"
+    | "lightGreen"
+    | "darkGray";
+
+  export interface DefaultTheme {
+    color: {
+      [key in DefaultThemeColorKey]: string;
+    };
+    boxShadow: {
+      [key in DefaultShadowKey]: string;
+    };
+  }
+}
